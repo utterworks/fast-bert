1944b14fee003cc2702ae86e8911f0a1d6be8a11
Pawel-Kranzberg <pawel@kranzberg.co>
2019-05-28 09:33:26 +0200

    Added CLASSIFICATION_THRESHOLD value for relevant metrics

--- fast_bert/metrics.py
+++ fast_bert/metrics.py
@@ -3,6 +3,8 @@ from torch import Tensor
 from sklearn.metrics import roc_curve, auc, hamming_loss, accuracy_score
 import pdb
 
+CLASSIFICATION_THRESHOLD:float = 0.5 # Best keep it in [0.0, 1.0] range
+
 # def accuracy(out, labels):
 #     outputs = np.argmax(out, axis=1)
 #     return np.sum(outputs == labels)
@@ -18,7 +20,7 @@ def accuracy_multilabel(y_pred:Tensor, y_true:Tensor, sigmoid:bool=True):
     real_vals = np.argmax(y_true, axis=1)
     return np.mean(outputs.numpy() == real_vals.numpy())
 
-def accuracy_thresh(y_pred:Tensor, y_true:Tensor, thresh:float=0.5, sigmoid:bool=True):
+def accuracy_thresh(y_pred:Tensor, y_true:Tensor, thresh:float=CLASSIFICATION_THRESHOLD, sigmoid:bool=True):
     "Compute accuracy when `y_pred` and `y_true` are the same size."
     if sigmoid: y_pred = y_pred.sigmoid()
     return ((y_pred>thresh)==y_true.byte()).float().mean().item()
@@ -53,15 +55,15 @@ def roc_auc(y_pred: Tensor, y_true: Tensor):
     
     return roc_auc["micro"]
 
-def Hamming_loss(y_pred:Tensor, y_true:Tensor, sigmoid:bool = True, thresh:float = threshold, sample_weight = None):
+def Hamming_loss(y_pred:Tensor, y_true:Tensor, sigmoid:bool = True, thresh:float = CLASSIFICATION_THRESHOLD, sample_weight = None):
     if sigmoid: y_pred = y_pred.sigmoid()
     y_pred = (y_pred > thresh).float()
     return hamming_loss(y_true, y_pred, sample_weight = sample_weight)
 
-def Exact_Match_Ratio(y_pred:Tensor, y_true:Tensor, sigmoid:bool = True, thresh:float = threshold, normalize:bool = True, sample_weight = None):
+def Exact_Match_Ratio(y_pred:Tensor, y_true:Tensor, sigmoid:bool = True, thresh:float = CLASSIFICATION_THRESHOLD, normalize:bool = True, sample_weight = None):
     if sigmoid: y_pred = y_pred.sigmoid()
     y_pred = (y_pred > thresh).float()
     return accuracy_score(y_true, y_pred, normalize = normalize, sample_weight = sample_weight)
 
-def F1(y_pred:Tensor, y_true:Tensor, threshold:float = threshold):
+def F1(y_pred:Tensor, y_true:Tensor, threshold:float = CLASSIFICATION_THRESHOLD):
     return fbeta(y_pred, y_true, thresh = threshold, beta = 1)
