d2242dd615e400a38e9de192d605989591164ddc
kaushaltrivedi <kaushaltrivedi@me.com>
2019-07-20 22:19:58 +0100

    version increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from io import open
 from setuptools import setup, find_packages
 
 setup(name='fast_bert',
-      version='0.1.2',
+      version='0.1.3',
       description='AI Library using BERT',
       author='Kaushal Trivedi',
       author_email='kaushaltrivedi@me.com',
