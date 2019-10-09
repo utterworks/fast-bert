90338dae9bb334e53db8536b64b02c08d71e10e0
kaushaltrivedi <kaushaltrivedi@me.com>
2019-10-09 10:24:26 +0100

    minor fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
     install_requires = f.read().strip().split('\n')
 
 setup(name='fast_bert',
-      version='1.4.2',
+      version='1.4.3',
       description='AI Library using BERT',
       author='Kaushal Trivedi',
       author_email='kaushaltrivedi@me.com',
