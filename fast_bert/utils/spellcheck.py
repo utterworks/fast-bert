9ede6a0aeb1e062513bae1033afcd695d370b2ca
Dom Hudson <dom.hudson@thoughtriver.com>
2019-10-20 13:27:06 +0100

    Refactoring spell check

--- fast_bert/utils/spellcheck.py
+++ fast_bert/utils/spellcheck.py
@@ -3,11 +3,34 @@ import requests
 
 
 class BingSpellCheck(object):
+
+    ENDPOINT = 'https://api.cognitive.microsoft.com/bing/v7.0/SpellCheck'
+
     def __init__(self, key):
+        """ Constructor.
+
+        :param str key:
+        :return void:
+        """
         self.api_key = key
-        self.endpoint = "https://api.cognitive.microsoft.com/bing/v7.0/SpellCheck"
 
     def spell_check(self, text, mode='spell'):
+        """ Correct spelling in a string.
+
+        :param str text:
+        :param str mode:
+        :return str:
+        """
+        flagged_tokens = self._get_flagged_tokens(text, mode)
+        return self._replace_flagged_tokens(text, flagged_tokens)
+
+    def _get_flagged_tokens(self, text, mode):
+        """ Make the request to Bing to get spell corrections for the given string.
+
+        :param str text:
+        :param str mode:
+        :return dict:
+        """
         data = {'text': text}
 
         params = {
@@ -19,15 +42,24 @@ class BingSpellCheck(object):
             'Content-Type': 'application/x-www-form-urlencoded',
             'Ocp-Apim-Subscription-Key': self.api_key,
         }
+
         response = requests.post(
-            self.endpoint, headers=headers, params=params, data=data)
+            self.ENDPOINT,
+            headers=headers,
+            params=params,
+            data=data
+        )
 
-        corrected_spells = response.json()
+        return response.json()['flaggedTokens']
 
-        flaggedTokens = corrected_spells['flaggedTokens']
+    def _replace_flagged_tokens(text, flagged_tokens):
+        """ Iterate through flagged tokens and replace them with the suggested tokens.
 
-        for flagged in flaggedTokens:
-            text = text.replace(
-                flagged['token'], flagged['suggestions'][0]['suggestion'])
+        :param str text:
+        :param list flagged_tokens:
+        :return str:
+        """
+        for flagged_ in flagged_tokens:
+            text = text.replace(flagged['token'], flagged['suggestions'][0]['suggestion'])
 
         return text
