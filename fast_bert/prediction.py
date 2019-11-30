5ee676b7db8add69f0b741f0fed95f1b558ae9f0
Aaron Briel <aaronbriel@gmail.com>
2019-11-30 14:35:58 -0600

    parameterized pretrained_model to distinguish from model_path for tokenizer initialization

--- fast_bert/prediction.py
+++ fast_bert/prediction.py
@@ -28,13 +28,19 @@ MODEL_CLASSES = {
 
 class BertClassificationPredictor(object):
 
-    def __init__(self, model_path, label_path, multi_label=False, model_type='bert', do_lower_case=True):
+    def __init__(self,
+                 model_path,
+                 label_path,
+                 pretrained_model='bert-base-uncased',
+                 multi_label=False,
+                 model_type='bert',
+                 do_lower_case=True):
         self.model_path = model_path
         self.label_path = label_path
+        self.pretrained_model = pretrained_model
         self.multi_label = multi_label
         self.model_type = model_type
         self.do_lower_case = do_lower_case
-
         self.learner = self.get_learner()
 
     def get_learner(self):
@@ -42,7 +48,7 @@ class BertClassificationPredictor(object):
         _, _, tokenizer_class = MODEL_CLASSES[self.model_type]
         # instantiate the new tokeniser object using the tokeniser name
         tokenizer = tokenizer_class.from_pretrained(
-            self.model_path, do_lower_case=self.do_lower_case)
+            self.pretrained_model, do_lower_case=self.do_lower_case)
 
         if torch.cuda.is_available():
             device = torch.device('cuda')
