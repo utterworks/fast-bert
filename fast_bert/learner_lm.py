ea73c4a09c47e8aba390e9eefca90bf7095e72cd
Shukanat <35403736+Shukanat@users.noreply.github.com>
2020-01-15 13:48:37 +0100

    Update learner_lm.py

--- fast_bert/learner_lm.py
+++ fast_bert/learner_lm.py
@@ -10,13 +10,14 @@ from .learner_util import Learner
 
 from .data_lm import BertLMDataBunch
 
-from transformers import (WEIGHTS_NAME, BertConfig, BertForMaskedLM, RobertaConfig, RobertaForMaskedLM, DistilBertConfig, DistilBertForMaskedLM)
+from transformers import (WEIGHTS_NAME, BertConfig, BertForMaskedLM, RobertaConfig, RobertaForMaskedLM, DistilBertConfig, DistilBertForMaskedLM, CamembertConfig, CamembertForMaskedLM)
 
 
 MODEL_CLASSES = {
     'bert': (BertConfig, BertForMaskedLM),
     'roberta': (RobertaConfig, RobertaForMaskedLM),
-    'distilbert': (DistilBertConfig, DistilBertForMaskedLM)
+    'distilbert': (DistilBertConfig, DistilBertForMaskedLM),
+    'camembert-base': (CamembertConfig, CamembertForMaskedLM)
 }
 
 class BertLMLearner(Learner):
