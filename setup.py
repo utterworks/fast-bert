baf1095c7f3ef427b50868823b1f6b66fb4ceed9
Kaushal Trivedi <kaushal.trivedi@utterworks.com>
2020-10-16 21:35:13 +0100

    minor updates

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="fast_bert",
-    version="1.9.2",
+    version="1.9.3",
     description="AI Library using BERT",
     author="Kaushal Trivedi",
     author_email="kaushaltrivedi@me.com",
