228c4386d2a669d5ba317f0d97e60583a056397a
Matt <washcycle@users.noreply.github.com>
2020-10-22 08:46:46 -0500

    Update learner_cls.py

--- fast_bert/learner_cls.py
+++ fast_bert/learner_cls.py
@@ -193,7 +193,7 @@ class BertLearner(Learner):
             is_fp16 = False
 
         model = load_model(
-            dataBunch, pretrained_path, finetuned_wgts_path, device, multi_label
+            dataBunch, pretrained_path, finetuned_wgts_path, device, multi_label, pos_weight, weight
         )
 
         return BertLearner(
