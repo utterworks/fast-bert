0ede72db097adbaa8566962dbdfc1f80ce16dfcc
Kaushal Trivedi <kaushal.trivedi@utterworks.com>
2022-04-11 09:20:37 +0000

    update setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="fast_bert",
-    version="1.9.15",
+    version="2.0.0",
     description="AI Library using BERT",
     author="Kaushal Trivedi",
     author_email="kaushaltrivedi@me.com",
