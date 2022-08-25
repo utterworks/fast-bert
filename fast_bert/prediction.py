ba6e1cb51d28b79b3375c670164d7157e0d5f3ba
Kaushal Trivedi <kaushal.trivedi@utterworks.com>
2022-08-25 20:49:07 +0000

    update NER code

--- fast_bert/prediction.py
+++ fast_bert/prediction.py
@@ -3,6 +3,9 @@ from pathlib import Path
 
 from .onnx_helper import load_model
 
+from .learner_ner import group_entities
+from .data_ner import get_labels
+
 from transformers import AutoTokenizer
 import numpy as np
 
@@ -206,6 +209,92 @@ class BertNERPredictor(object):
             text, group=group, exclude_entities=exclude_entities
         )
         return predictions
+
+
+class BertOnnxNERPredictor(object):
+    def __init__(
+        self,
+        model_path,
+        label_path,
+        model_name="model.onnx",
+        model_type="bert",
+        use_fast_tokenizer=True,
+        do_lower_case=True,
+        device=None,
+    ):
+        if device is None:
+            device = (
+                torch.device("cuda")
+                if torch.cuda.is_available()
+                else torch.device("cpu")
+            )
+
+        self.model_path = model_path
+        self.label_path = label_path
+        self.model_type = model_type
+        self.do_lower_case = do_lower_case
+        self.device = device
+        self.labels = []
+
+        # Use auto-tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_path, use_fast=use_fast_tokenizer
+        )
+
+        self.labels = get_labels(str(label_path / "labels.txt"))
+
+        self.model = load_model(Path(self.model_path) / model_name)
+
+    def predict(self, text, group=True, exclude_entities=["O"]):
+        # Inputs are provided through numpy array
+        tokens = self.tokenizer.tokenize(
+            self.tokenizer.decode(self.tokenizer.encode(text))
+        )
+
+        model_inputs = self.tokenizer(text, return_tensors="pt")
+        inputs_onnx = {k: v.cpu().detach().numpy() for k, v in model_inputs.items()}
+        outputs = self.model.run(None, inputs_onnx)[0]
+        outputs = softmax(outputs)
+
+        predictions = np.argmax(outputs, axis=2)
+
+        preds = [
+            (token, self.labels[prediction], output[prediction])
+            for token, output, prediction in zip(tokens, outputs[0], predictions[0])
+        ][1:-1]
+
+        preds = [
+            {
+                "index": index,
+                "word": prediction[0],
+                "entity": prediction[1],
+                "score": prediction[2],
+            }
+            for index, prediction in enumerate(preds)
+        ]
+
+        if group is True:
+            preds = group_entities(preds)
+
+        out_preds = []
+        for pred in preds:
+            if pred["entity"] not in exclude_entities:
+                try:
+                    pred["entity"] = pred["entity"].split("-")[1]
+                except Exception:
+                    pass
+
+                out_preds.append(pred)
+
+        return out_preds
+
+    def predict_batch(self, texts, group=True, exclude_entities=["O"]):
+        predictions = []
+
+        for text in texts:
+            pred = self.predict(text, group=group, exclude_entities=exclude_entities)
+            if pred:
+                predictions.append({"text": text, "results": pred})
 
 
 def softmax(x: np.ndarray, axis: int = -1) -> np.ndarray:
